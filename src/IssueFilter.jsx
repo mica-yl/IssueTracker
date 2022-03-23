e7f3a3430891a3e5a733dfe74b3df62350072220
mica-yl <7644696+mica-yl@users.noreply.github.com>
2022-03-23 14:53:50 +0200

    adding propT-types

--- src/IssueFilter.jsx
+++ src/IssueFilter.jsx
@@ -1,18 +1,42 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+function Separator() {
+  return <span> | </span>;
+}
 
 export default function IssueFilter(props) {
-  const { filters: { status }, onRefresh } = props;
+  const { filters: { status } } = props;
   return (
     <div>
-      <h2>filters:</h2>
-      <p>status :</p>
-      {status.map((aStatus) => (
-        <div>
-          <Link to={`?status=${aStatus}`} onClick={onRefresh}>{aStatus}</Link>
-          {' '}
-        </div>
-      ))}
+      <p>
+        filters:
+        {' '}
+        <Link to={{ pathname: '.', search: '' }}>
+          clear
+        </Link>
+        <br />
+        status :
+        {' '}
+        {status.map((aStatus, i, arr) => (
+          <span key={aStatus}>
+            <Link to={`?status=${aStatus}`}>
+              {aStatus}
+            </Link>
+            {i < arr.length - 1 ? <Separator /> : ''}
+          </span>
+        ))}
+      </p>
     </div>
   );
 }
+IssueFilter.defaultProps = {
+  filters: [],
+};
+IssueFilter.propTypes = {
+  filters: PropTypes.shape({
+    status: PropTypes.arrayOf(PropTypes.string.isRequired).isRequired,
+
+  }),
+};
