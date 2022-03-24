9c3ba33c5bdea8ba3db1c29f00d729149126fbda
mica-yl <7644696+mica-yl@users.noreply.github.com>
2022-03-24 11:18:38 +0200

    eslint spaces fix

--- webpack.config.js
+++ webpack.config.js
@@ -1,49 +1,48 @@
 const path = require('path');
 
-
 module.exports = {
-    mode: 'development',
-    entry: {
-        app: [path.resolve(__dirname, './src/App.jsx')],
-    },
-    output: {
-        filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'static/bundle/'),
-        publicPath:'/bundle/',
-    },
-    optimization: {
-        splitChunks: {
-            cacheGroups: {
-                commons: {
-                    test: /[\\/]node_modules[\\/]/,
-                    name: 'vendors',
-                    chunks: 'all',
-                },
-            },
+  mode: 'development',
+  entry: {
+    app: [path.resolve(__dirname, './src/App.jsx')],
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'static/bundle/'),
+    publicPath: '/bundle/',
+  },
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        commons: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'all',
         },
+      },
     },
-    module: {
-        rules: [
-            {
-                test: /\.jsx$/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-react', '@babel/preset-env',],
-                    },
-                },
-            },
-        ],
-    },
-    devServer: {
-        port: 8082,
-        static: false,
-        compress: true,
-        hot: true,
-        proxy: {
-            '*': 'http://localhost:8081',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react', '@babel/preset-env'],
+          },
         },
+      },
+    ],
+  },
+  devServer: {
+    port: 8082,
+    static: false,
+    compress: true,
+    hot: true,
+    proxy: {
+      '*': 'http://localhost:8081',
     },
-    plugins:[],
-    devtool:'source-map',
-}
+  },
+  plugins: [],
+  devtool: 'source-map',
+};
