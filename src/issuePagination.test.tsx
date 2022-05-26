9ff9c6e1bd1a5312d83c9ecc368be06c92ea4dbb
mica-yl <7644696+mica-yl@users.noreply.github.com>
2022-05-26 10:53:24 +0200

    fix jest eslintrc

--- src/issuePagination.test.tsx
+++ src/issuePagination.test.tsx
@@ -1,9 +1,12 @@
+import { describe, expect, test } from '@jest/globals';
 import { divide } from './IssuePagination';
 
-test('fail', () => {
-  expect(divide(1, 2)).not.toEqual(4);
-});
+describe('trivial tests', () => {
+  test('fail', () => {
+    expect(divide(1, 2)).not.toEqual(4);
+  });
 
-test('pass', () => {
-  expect(divide(1, 2)).toEqual([]);
+  test('pass', () => {
+    expect(divide(1, 2)).toEqual([]);
+  });
 });
