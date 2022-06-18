aec329811a8f3f600688848e5ca6a4149153ac3d
mica-yl <7644696+mica-yl@users.noreply.github.com>
2022-06-18 09:41:54 +0200

    [client-side] fix navbar side spaces

--- src/App/Header/Header.tsx
+++ src/App/Header/Header.tsx
@@ -20,63 +20,60 @@ export default function Header(props:{API:API}) {
 
   return (
     <Navbar bg="light" variant="light" className="fluid">
-
-      <Container>
-        <Navbar.Brand>Issue Tracker</Navbar.Brand>
-        <Nav variant="tabs" className="me-auto">
-          <Nav.Item>
-            <LinkContainer to="/issues">
-              <Nav.Link>
-                Issues
-              </Nav.Link>
-            </LinkContainer>
-          </Nav.Item>
-          <Nav.Item>
-            <LinkContainer to="/reports">
-              <Nav.Link>
-                Reports
-              </Nav.Link>
-            </LinkContainer>
-          </Nav.Item>
-        </Nav>
-        <Nav className="pull-right">
-          <ConditionalRender condition={user.signedIn}>
-            <IssueAddNavItem onSubmit={createIssue}>
-              <Plus />
-              {' '}
-              Create Issue
-            </IssueAddNavItem>
-          </ConditionalRender>
-          {/* <UserNavItem /> */}
-          <NavDropdown
-            bsPrefix="no-caret"
-            align="end"
-            title={(
+      <Navbar.Brand>Issue Tracker</Navbar.Brand>
+      <Nav variant="tabs" className="me-auto">
+        <Nav.Item>
+          <LinkContainer to="/issues">
+            <Nav.Link>
+              Issues
+            </Nav.Link>
+          </LinkContainer>
+        </Nav.Item>
+        <Nav.Item>
+          <LinkContainer to="/reports">
+            <Nav.Link>
+              Reports
+            </Nav.Link>
+          </LinkContainer>
+        </Nav.Item>
+      </Nav>
+      <Nav className="pull-right">
+        <ConditionalRender condition={user.signedIn}>
+          <IssueAddNavItem onSubmit={createIssue}>
+            <Plus />
+            {' '}
+            Create Issue
+          </IssueAddNavItem>
+        </ConditionalRender>
+        {/* <UserNavItem /> */}
+        <NavDropdown
+          bsPrefix="no-caret"
+          align="end"
+          title={(
               // needs adaptive resizing
-              <Image src={user.picture} fluid roundedCircle width="50px" />
+            <Image src={user.picture} fluid roundedCircle width="50px" />
         )}
-          >
-            <NavDropdown.Header>
-              {user.name}
-            </NavDropdown.Header>
-            <NavDropdown.Divider />
-            <ConditionalRender condition={user.signedIn}>
-              <SignOutNavDropDownItem
-                refresh={() => dispatchUser('refresh')}
-              >
-                Sign Out
-              </SignOutNavDropDownItem>
-            </ConditionalRender>
-            <ConditionalRender condition={!user.signedIn}>
-              <LinkContainer to="/login">
-                <NavDropdown.Item>
-                  Log In
-                </NavDropdown.Item>
-              </LinkContainer>
-            </ConditionalRender>
-          </NavDropdown>
-        </Nav>
-      </Container>
+        >
+          <NavDropdown.Header>
+            {user.name}
+          </NavDropdown.Header>
+          <NavDropdown.Divider />
+          <ConditionalRender condition={user.signedIn}>
+            <SignOutNavDropDownItem
+              refresh={() => dispatchUser('refresh')}
+            >
+              Sign Out
+            </SignOutNavDropDownItem>
+          </ConditionalRender>
+          <ConditionalRender condition={!user.signedIn}>
+            <LinkContainer to="/login">
+              <NavDropdown.Item>
+                Log In
+              </NavDropdown.Item>
+            </LinkContainer>
+          </ConditionalRender>
+        </NavDropdown>
+      </Nav>
     </Navbar>
   );
 }
