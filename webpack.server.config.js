367bb5ba14beab0f7b6a8a3cc686f9ad2409716b
mica-yl <7644696+mica-yl@users.noreply.github.com>
2022-08-03 12:21:46 +0200

    [webpack] fix building server + production mode

--- webpack.server.config.js
+++ webpack.server.config.js
@@ -1,49 +1,78 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 /* eslint-disable import/no-extraneous-dependencies */
 const path = require('path');
-const nodeExternals = require('webpack-node-externals');
 const webpack = require('webpack');
+// plugins
+const nodeExternals = require('webpack-node-externals');
 const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 const RunInMemoryPlugin = require('RunInMemoryPlugin');
 
-const extensions = ['.js', '.jsx', '.ts', '.tsx', '.json'];
-// TODO optional run in memory 
-// TODO - to .
-module.exports = {
-  mode: 'development',
-  target: 'node',
-  externalsPresets: { node: true },
-  externals: nodeExternals(),
-  entry: {
-    server: [path.resolve(__dirname, './server/index.ts'), './node_modules/webpack/hot/poll?1000'],
+/**
+ * @typedef {{
+ * production:boolean,
+ * inMem:boolean,
+ * }} wepbackEnv
+ * @param {wepbackEnv} env
+ * @param {Record<string,any>} argv
+ * @returns {webpack.Configuration}webpackServerConfig
+ */
+function serverConfig(env, argv) {
+  const production = env.production || argv.mode === 'production';
+  const development = !production;
+  const extensions = ['.js', '.jsx', '.ts', '.tsx', '.json'];
+  /**  @type {webpack.Configuration}  */
+  const config = {
+    mode: production ? 'production' : 'development',
+    target: 'node',
+    externalsPresets: { node: true },
+    externals: nodeExternals(),
+    entry: {
+      server: [path.resolve(__dirname, './server/index.ts')],
 
-  },
-  output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist/'),
-    libraryTarget: 'commonjs2',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.[tj]sx?$/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/env', '@babel/react', '@babel/typescript'],
+    },
+    output: {
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'dist/'),
+      libraryTarget: 'commonjs2',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.[tj]sx?$/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/env', '@babel/react', '@babel/typescript'],
+            },
           },
+          // exclude: /[\\/]modules[\\/]/,
         },
-        // exclude: /[\\/]modules[\\/]/,
-      },
-    ],
-  },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new RunInMemoryPlugin({ requireFile: path.join(__dirname, './dist/server.bundle.js') }),
-  ],
-  devtool: 'source-map',
-  resolve: {
-    extensions,
-    plugins: [new TsconfigPathsPlugin({ extensions })],
-  },
-};
+      ],
+    },
+    plugins: [],
+    devtool: development ? 'source-map' : 'eval',
+    resolve: {
+      extensions,
+      plugins: [new TsconfigPathsPlugin({ extensions })],
+    },
+  };
+
+  if (development) {
+    config.entry.server.push(path.join(__dirname, './node_modules/webpack/hot/poll?1000'));
+    config.plugins.push(
+      new webpack.HotModuleReplacementPlugin(),
+    );
+    // build in memory
+    if (env.inMem) {
+      config.plugins.push(
+        new RunInMemoryPlugin({
+          requireFile: path.join(__dirname, './dist/server.bundle.js')
+        }),
+      );
+    }
+  }
+
+  return config;
+}
+
+module.exports = serverConfig;
